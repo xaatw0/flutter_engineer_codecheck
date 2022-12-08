4af68e6314c485ad391303d8e21b0236e9480e98
sakushin <sakushin.udemy@gmail.com>
2022-12-08 18:38:12 +0900

    DIの設定をした

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_engineer_codecheck/domain/repositories/git_repository.dart';
+import 'package:flutter_engineer_codecheck/infrastructures/github_repositories/github_repository.dart';
 import 'package:flutter_engineer_codecheck/ui/my_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 
 /// 起点クラス
 void main() {
+  initDI();
   runApp(
     const ProviderScope(
       child: MyApp(),
     ),
   );
+}
+
+/// DIの設定
+void initDI() {
+  GetIt.I.registerSingleton<GitRepository>(GithubRepository());
 }
--- lib/ui/pages/search_result_page/search_result_page_vm.dart
+++ lib/ui/pages/search_result_page/search_result_page_vm.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_engineer_codecheck/domain/entities/git_repository_data.dart';
-import 'package:flutter_engineer_codecheck/infrastructures/github_repositories/github_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../domain/repositories/git_repository.dart';
 
 /// 剣客結果を表示するページのViewModel
 class SearchResultPageVm {
   final _repositoryData = FutureProvider.autoDispose
       .family<List<GitRepositoryData>, String>((ref, keyword) async {
-    final result = GithubRepository().search(keyword);
+    final result = GetIt.I.get<GitRepository>().search(keyword);
     return result;
   });
 
