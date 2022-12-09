ef68f0689c98548e3e9ed0cf6433d39b6cd93426
sakushin <sakushin.udemy@gmail.com>
2022-12-09 17:09:41 +0900

    レポジトリの詳細を表示するページをデータから生成されるようにした

--- lib/ui/pages/repository_detail_page/repository_detail_page.dart
+++ lib/ui/pages/repository_detail_page/repository_detail_page.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_engineer_codecheck/ui/widgets/atoms/owner_image.dart';
+import 'package:flutter_engineer_codecheck/domain/entities/git_repository_data.dart';
 import 'package:flutter_engineer_codecheck/ui/widgets/templates/day_night_template.dart';
 
+import '../../widgets/molecules/owner_clip.dart';
 import '../../widgets/molecules/repository_detail_column.dart';
+import '../../widgets/molecules/repository_detail_description.dart';
 
 /// レポジトリの詳細を表示するページ
 class RepositoryDetailPage extends StatefulWidget {
-  const RepositoryDetailPage({Key? key}) : super(key: key);
+  const RepositoryDetailPage(this.data, {super.key});
 
   static const path = '/repository';
+
+  final GitRepositoryData data;
 
   @override
   State<RepositoryDetailPage> createState() => _RepositoryDetailPageState();
@@ -17,74 +21,37 @@ class RepositoryDetailPage extends StatefulWidget {
 class _RepositoryDetailPageState extends State<RepositoryDetailPage> {
   @override
   Widget build(BuildContext context) {
+    final repositoryData = widget.data;
+    final description = repositoryData.repositoryDescription() ?? '';
     return DayNightTemplate(children: [
-      Center(
-        child: Container(
-          height: 135,
-          width: 135,
-          padding: EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 8),
-            ],
-            gradient: LinearGradient(
-              //stops: [0, 1],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.grey, Colors.white70],
-            ),
-          ),
-          child: ClipOval(
-            child: OwnerImage(
-              url: 'https://avatars.githubusercontent.com/u/14101776?v=4',
-            ),
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20.0,
-      ),
+      // オーナー画像
+      OwnerClip(repositoryData.ownerIconUrl),
+      const SizedBox(height: 20.0),
+      // レポジトリ名
       Center(
         child: Text(
-          "Flutter",
+          repositoryData.repositoryName(),
           style: Theme.of(context).textTheme.displaySmall,
         ),
       ),
-      SizedBox(
-        height: 20.0,
-      ),
+      const SizedBox(height: 20.0),
+      // Star, Wather, Fork, Issue の一覧
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          RepositoryDetailColumn('Star', 146985),
-          RepositoryDetailColumn('Watcher', 146985),
-          RepositoryDetailColumn('Fork', 146985),
-          RepositoryDetailColumn('Issue', 146985),
+          RepositoryDetailColumn('Star', repositoryData.countStar()),
+          RepositoryDetailColumn('Watcher', repositoryData.countWatcher()),
+          RepositoryDetailColumn('Fork', repositoryData.countFork()),
+          RepositoryDetailColumn('Issue', repositoryData.countIssue()),
         ],
       ),
-      SizedBox(
+      const SizedBox(
         height: 20.0,
       ),
-      Center(
-        child: Text(
-          'Description',
-          textAlign: TextAlign.left,
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-      ),
-      Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Text(
-            "Flutter makes it easy and fast to build beautiful apps for mobile and beyond",
-            textAlign: TextAlign.left,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-        ),
+      // 詳細説明
+      Visibility(
+        visible: description.isNotEmpty,
+        child: RepositoryDetailDescription(description: description),
       ),
     ]);
   }
--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/ui/pages/repository_detail_page/repository_detail_page.dart';
 import 'package:flutter_engineer_codecheck/ui/pages/search_page/search_page.dart';
 import 'package:flutter_engineer_codecheck/ui/pages/search_result_page/search_result_page.dart';
+
 import 'package:go_router/go_router.dart';
+import '../domain/entities/git_repository_data.dart';
+import '../domain/value_objects/count_fork.dart';
+import '../domain/value_objects/count_issue.dart';
+import '../domain/value_objects/count_star.dart';
+import '../domain/value_objects/count_watcher.dart';
+import '../domain/value_objects/owner_icon_url.dart';
+import '../domain/value_objects/project_language.dart';
+import '../domain/value_objects/repository_description.dart';
+import '../domain/value_objects/repository_name.dart';
 
 /// アプリ全体の遷移用のルート設定
 final router = GoRouter(
@@ -34,10 +44,23 @@ final router = GoRouter(
               );
       },
     ),
+    // レポジトリ詳細ページ
     GoRoute(
         path: '/', //RepositoryDetailPage.path,
         pageBuilder: (BuildContext context, GoRouterState? state) {
-          return MaterialPage(child: RepositoryDetailPage());
+          final data = GitRepositoryData(
+            repositoryName: RepositoryName('flutter'),
+            ownerIconUrl: OwnerIconUrl(
+                'https://avatars.githubusercontent.com/u/14101776?v=4'),
+            projectLanguage: ProjectLanguage('Dart'),
+            repositoryDescription: RepositoryDescription(
+                'Flutter makes it easy and fast to build beautiful apps for mobile and beyond'),
+            countStar: CountStar(146985),
+            countWatcher: CountWatcher(146985),
+            countFork: CountFork(23912),
+            countIssue: CountIssue(11313),
+          );
+          return MaterialPage(child: RepositoryDetailPage(data));
         })
   ],
 );
