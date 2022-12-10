205e5ae1ca31c5d014ca27fc1a0edcf15de01f4b
sakushin <sakushin.udemy@gmail.com>
2022-12-10 18:39:38 +0900

    GitResponsiveのソート方法を設定した

--- lib/domain/repositories/git_repository.dart
+++ lib/domain/repositories/git_repository.dart
@@ -1,5 +1,29 @@
 import '../entities/git_repository_data.dart';
 
+/// Gitの検索結果の表示順
+enum SortMethod {
+  /// ベストマッチ順
+  bestMatch,
+
+  /// スターが多い順
+  starAsc,
+
+  /// スターが少ない順
+  starDesc,
+
+  /// フォークが多い順
+  forkAsc,
+
+  /// フォークが少ない順
+  forkDesc,
+
+  /// 更新日時が新しい順
+  recentlyUpdated,
+
+  /// 更新日時が古い順
+  leastRecentlyUpdate,
+}
+
 /// Gitからデータを取得するためのレポジトリクラス (Gitのソース解離のレポジトリと間違えやすい)。
 /// Githubだけではなく、他のGitでデータ取得をするケースも想定して、抽象クラスを作成する。
 abstract class GitRepository {
@@ -8,6 +32,7 @@ abstract class GitRepository {
   Future<List<GitRepositoryData>> search(
     String keyword, {
     int page = 1,
+    SortMethod sortMethod,
   });
 
   /// 最初の1ページ目のインデックス
--- lib/infrastructures/github_repositories/github_repository.dart
+++ lib/infrastructures/github_repositories/github_repository.dart
@@ -17,22 +17,79 @@ class GithubRepository implements GitRepository {
   /// パラメータのページ数の置換用文字列
   static const kParamPage = '<page>';
 
-  /// Github APIへアクセスするためのURL
-  static const String apiUrl =
+  /// パラメータのソート方法の置換用文字列
+  static const kParamSort = '<sort>';
+
+  /// パラメータのソートの昇順・降順の置換用文字列
+  static const kParamOrder = '<order>';
+
+  /// Github APIへソート条件をベストマッチ(デフォルト)でアクセスするためのURL
+  static const String kApiUrlForBestMatch =
       'https://api.github.com/search/repositories?q=$kParamKeyword&page=$kParamPage';
+
+  /// Github APIへソート条件をつけてアクセスするためのURL
+  static const String kApiUrl =
+      '$kApiUrlForBestMatch&sort=$kParamSort&order=$kParamOrder';
+
+  /// 「検索結果の表示順」のソートの並び順とパラメータのキーワードとの対応表
+  static const kMapOrder = <String, List<SortMethod>>{
+    'asc': [
+      SortMethod.starAsc,
+      SortMethod.forkAsc,
+      SortMethod.recentlyUpdated,
+    ],
+    'desc': [
+      SortMethod.starDesc,
+      SortMethod.forkDesc,
+      SortMethod.leastRecentlyUpdate
+    ],
+  };
+
+  /// 「検索結果の表示順」のソートの方法とパラメータのキーワードとの対応表
+  static const kMapSort = <String, List<SortMethod>>{
+    'star': [SortMethod.starAsc, SortMethod.starDesc],
+    'fork': [SortMethod.forkAsc, SortMethod.forkDesc],
+    'update': [
+      SortMethod.recentlyUpdated,
+      SortMethod.leastRecentlyUpdate,
+    ],
+  };
 
   @override
   Future<List<GitRepositoryData>> search(
     String keyword, {
     int page = 1,
+    SortMethod sortMethod = SortMethod.bestMatch,
   }) async {
     // TODO 並び順、ページ数などへの対応
-    final uri = apiUrl
+    final uri = kApiUrl
         .replaceFirst(kParamKeyword, keyword)
         .replaceFirst(kParamPage, page.toString());
     final apiUri = Uri.parse(uri);
     http.Response response = await http.get(apiUri);
     return fromJson(response.body).toList();
+  }
+
+  /// キーワード、ページ、ソート条件に基づいた検索用のURLを発行する
+  String getSearchUrl(String keyword, int page, SortMethod sortMethod) {
+    if (sortMethod == SortMethod.bestMatch) {
+      return kApiUrlForBestMatch
+          .replaceFirst(kParamKeyword, keyword)
+          .replaceFirst(kParamPage, page.toString());
+    } else {
+      final paramSort = kMapSort.keys
+          .where((key) => kMapSort[key]!.contains(sortMethod))
+          .first;
+      final paramOrder = kMapOrder.keys
+          .where((key) => kMapOrder[key]!.contains(sortMethod))
+          .first;
+
+      return kApiUrl
+          .replaceFirst(kParamKeyword, keyword)
+          .replaceFirst(kParamPage, page.toString())
+          .replaceFirst(kParamSort, paramSort)
+          .replaceFirst(kParamOrder, paramOrder);
+    }
   }
 
   Iterable<GitRepositoryData> fromJson(String jsonData) {
