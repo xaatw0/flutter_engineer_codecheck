b24ba3028c24ff6c86cc4509df4e8c3ffadd3f7e
sakushin <sakushin.udemy@gmail.com>
2022-12-11 07:12:28 +0900

    多言語化対応用の設定を追加した

--- lib/ui/my_app.dart
+++ lib/ui/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/ui/router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'app_theme.dart';
 
 /// テーマの設定と切り替え。
@@ -13,11 +13,19 @@ class MyApp extends StatelessWidget {
     return Consumer(
       builder: (context, ref, child) => MaterialApp.router(
         title: 'Flutter Demo',
+
+        // テーマ
         themeMode: ref.watch(AppTheme.themeMode),
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.dartTheme,
+
+        // GoRouter
         routerDelegate: router.routerDelegate,
         routeInformationParser: router.routeInformationParser,
+
+        // 多言語対応
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
       ),
     );
   }
