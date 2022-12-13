6f8ceaa20ca1d29f64a061f4fed1b327a5462b3a
sakushin <sakushin.udemy@gmail.com>
2022-12-13 20:40:58 +0900

    画面を横向きにしてキーボードが表示されたとき、画面が表示しきれない。そのため、パディングを減らすなどして対応した

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ void main() {
   SystemChrome.setPreferredOrientations(
     orientations,
   );
-  print('orientations length: ${orientations.length} ${orientations[0].name}');
   runApp(
     const ProviderScope(
       child: MyApp(),
--- lib/ui/widgets/templates/day_night_template.dart
+++ lib/ui/widgets/templates/day_night_template.dart
@@ -4,34 +4,53 @@ import 'package:flutter_engineer_codecheck/ui/widgets/organisms/theme_switcher.d
 import '../../../domain/string_resources.dart';
 
 /// ライトテーマとダークテーマの切り替えボタンのあるテンプレート。
-class DayNightTemplate extends StatelessWidget {
+class DayNightTemplate extends StatefulWidget {
   const DayNightTemplate({
     super.key,
     this.title,
     required this.child,
     this.isAppBarShown = true,
+    this.hasPadding = true,
   });
 
+  /// 中身のWidget
   final Widget child;
+
+  /// アップバーに表示するタイトル
   final String? title;
 
   /// アップバーを表示するか(true:表示 false:非表示)
   /// 画面を横向きにしたときのスペース確保のために非表示にできる
   final bool isAppBarShown;
 
+  /// 標準のパディングをつけるか
+  final bool hasPadding;
+
+  @override
+  State<DayNightTemplate> createState() => _DayNightTemplateState();
+}
+
+class _DayNightTemplateState extends State<DayNightTemplate> {
+  static const normalPaddingSize = 16.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: isAppBarShown
+      appBar: widget.isAppBarShown && widget.hasPadding
           ? AppBar(
-              title: Text(title ?? StringResources.kEmpty),
+              title: Text(widget.title ?? StringResources.kEmpty),
               actions: const [ThemeSwitcher()],
             )
           : null,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: child,
+          padding: EdgeInsets.fromLTRB(
+            normalPaddingSize,
+            widget.hasPadding ? normalPaddingSize : 4,
+            normalPaddingSize,
+            0,
+          ),
+          child: widget.child,
         ),
       ),
     );
