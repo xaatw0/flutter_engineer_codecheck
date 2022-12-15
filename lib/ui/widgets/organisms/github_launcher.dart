d77b799bd93c1f6a8e357ded1785ad9842f1a2f3
sakushin <sakushin.udemy@gmail.com>
2022-12-15 20:30:00 +0900

    Githubを開く処理を追加した

--- lib/ui/widgets/organisms/github_launcher.dart
+++ lib/ui/widgets/organisms/github_launcher.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_engineer_codecheck/ui/app_theme.dart' as app_theme;
 import 'package:flutter_engineer_codecheck/ui/widgets/atoms/github_icon.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_engineer_codecheck/ui/app_theme.dart' as app_theme;
+import 'package:url_launcher/url_launcher.dart';
 
 /// Githubをブラウザで開くためのボタン
 class GithubLauncher extends StatelessWidget {
-  GithubLauncher(this.url, {super.key});
+  const GithubLauncher(this.url, {super.key});
 
   /// ブラウザで開くURL
   final String url;
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (_, WidgetRef ref, __) {
-      return GithubIcon(
-        size: 32,
-        isDarkMode: ref.watch(app_theme.themeMode) == ThemeMode.dark,
-      );
-    });
+    return Consumer(
+      builder: (_, WidgetRef ref, __) {
+        return IconButton(
+          icon: GithubIcon(
+            size: 32,
+            isDarkMode: ref.watch(app_theme.themeMode) == ThemeMode.dark,
+          ),
+          onPressed: _launchUrl,
+        );
+      },
+    );
+  }
+
+  /// ブラウザでGithubを開く
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('以下を開くことができませんでした $url');
+    }
   }
 }
