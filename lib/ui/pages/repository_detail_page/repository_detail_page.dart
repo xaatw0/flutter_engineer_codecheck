ea0e048796b9dcb057b6b33f2b79f97b91b9b0d1
sakushin <sakushin.udemy@gmail.com>
2022-12-16 18:35:15 +0900

    レポジトリ詳細ページを対応 ・OrientationBuilderで横サイズにテンプレートの制限を使うため、多くの場合縦向きと判断される。OrientationBuilderの使用をやめて、単純に幅を使用するようにした ・ブラウザを小さくすると表示できないため、スクロールできるようにした

--- lib/ui/pages/repository_detail_page/repository_detail_page.dart
+++ lib/ui/pages/repository_detail_page/repository_detail_page.dart
@@ -52,92 +52,87 @@ class RepositoryDetailPage extends StatelessWidget {
     // Widgetの場所で設定すると、向きで非表示になるので、データが取得できず例外になる。
     // そのため、ここで取得している。
     final columnsCount = context.responsive<int>(2, small: 4);
+    final isNarrow = MediaQuery.of(context).size.width < Responsive.breakMiddle;
 
     return DayNightTemplate(
-      child: OrientationBuilder(
-        builder: (BuildContext context, Orientation orientation) {
-          final isPortrait = orientation == Orientation.portrait;
-          return Column(
-            children: [
-              // レポジトリ名
-              Center(
-                child: AutoSizeText(
-                  repositoryData.repositoryName(),
-                  style: Theme.of(context).textTheme.displaySmall,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  minFontSize:
-                      // 最低でも項目の数字のフォントサイズ
-                      Theme.of(context).textTheme.titleLarge!.fontSize!,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // レポジトリ名
+            AutoSizeText(
+              repositoryData.repositoryName(),
+              style: Theme.of(context).textTheme.displaySmall,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              minFontSize:
+                  // 最低でも項目の数字のフォントサイズ
+                  Theme.of(context).textTheme.titleLarge!.fontSize!,
+            ),
+            SizedBox(height: isNarrow ? 20 : 10),
+            // オーナー画像
+            Row(
+              mainAxisAlignment:
+                  isNarrow ? MainAxisAlignment.center : MainAxisAlignment.start,
+              crossAxisAlignment: isNarrow
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Hero(
+                    tag: OwnerImage.kHeroKey +
+                        repositoryData.repositoryId().toString(),
+                    child: OwnerClip(repositoryData.ownerIconUrl),
+                  ),
                 ),
-              ),
-              SizedBox(height: isPortrait ? 20 : 10),
-              // オーナー画像
-              Row(
-                mainAxisAlignment: isPortrait
-                    ? MainAxisAlignment.center
-                    : MainAxisAlignment.start,
-                crossAxisAlignment: isPortrait
-                    ? CrossAxisAlignment.center
-                    : CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: Hero(
-                      tag: OwnerImage.kHeroKey +
-                          repositoryData.repositoryId().toString(),
-                      child: OwnerClip(repositoryData.ownerIconUrl),
+                Visibility(
+                  visible: !isNarrow,
+                  child: Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: [
+                        RepositoryDataGridView(
+                          columns: columns,
+                          axisCount: columnsCount,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: GithubLauncher(
+                            repositoryData.repositoryHtmlUrl(),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Visibility(
-                    visible: !isPortrait,
-                    child: Expanded(
-                      flex: 2,
-                      child: Column(
-                        children: [
-                          RepositoryDataGridView(
-                            columns: columns,
-                            axisCount: columnsCount,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: GithubLauncher(
-                              repositoryData.repositoryHtmlUrl(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
 
-              // Star, Watcher, Fork, Issue の一覧
-              Visibility(
-                visible: isPortrait,
-                child: RepositoryDataGridView(
-                  columns: columns,
-                  axisCount: columnsCount,
+            // Star, Watcher, Fork, Issue の一覧
+            Visibility(
+              visible: isNarrow,
+              child: RepositoryDataGridView(
+                columns: columns,
+                axisCount: columnsCount,
+              ),
+            ),
+            if (isNarrow)
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: GithubLauncher(
+                  repositoryData.repositoryHtmlUrl(),
                 ),
               ),
-              if (isPortrait)
-                Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: GithubLauncher(
-                    repositoryData.repositoryHtmlUrl(),
-                  ),
-                ),
-              SizedBox(height: isPortrait ? 20 : 0),
-              // 詳細説明
-              Visibility(
-                visible: description.isNotEmpty,
-                child: SingleChildScrollView(
-                  child: RepositoryDetailDescription(description: description),
-                ),
+            SizedBox(height: isNarrow ? 20 : 0),
+            // 詳細説明
+            Visibility(
+              visible: description.isNotEmpty,
+              child: SingleChildScrollView(
+                child: RepositoryDetailDescription(description: description),
               ),
-            ],
-          );
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
