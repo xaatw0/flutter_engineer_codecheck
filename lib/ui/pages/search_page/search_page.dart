7da2ee854d65e015fecf4980800e38c66e5aaacc
sakushin <sakushin.udemy@gmail.com>
2022-12-16 18:13:27 +0900

    要素の最大幅をテンプレートに持たせるようにした

--- lib/ui/pages/search_page/search_page.dart
+++ lib/ui/pages/search_page/search_page.dart
@@ -10,7 +10,6 @@ import 'package:get_it/get_it.dart';
 import '../../widgets/atoms/github_icon.dart';
 import '../../widgets/molecules/search_text_field.dart';
 import '../../widgets/organisms/moving_fadein_animation.dart';
-import '../../responsive.dart';
 
 /// 検索用のトップページのView
 class SearchPage extends ConsumerStatefulWidget {
@@ -46,70 +45,63 @@ class _SearchPageState extends ConsumerState<SearchPage> {
         child: DayNightTemplate(
           isAppBarShown: visibleWidgetLogic.hasPadding,
           hasPadding: visibleWidgetLogic.hasPadding,
-          child: Center(
-            child: Column(
-              children: [
-                // Githubのアイコン
-                Visibility(
-                  visible: visibleWidgetLogic.isLogoVisible,
-                  child: Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: MovingFadeinAnimation(
-                      child: GithubIcon(
-                        size: MediaQuery.of(context).size.height / 4,
-                        isDarkMode: _vm.isDarkMode,
-                      ),
+          child: Column(
+            children: [
+              // Githubのアイコン
+              Visibility(
+                visible: visibleWidgetLogic.isLogoVisible,
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: MovingFadeinAnimation(
+                    child: GithubIcon(
+                      size: MediaQuery.of(context).size.height / 4,
+                      isDarkMode: _vm.isDarkMode,
                     ),
                   ),
                 ),
+              ),
 
-                // 検索のキーワード入力
-                Semantics(
+              // 検索のキーワード入力
+              Semantics(
+                container: true,
+                label: AppLocalizations.of(context).searchExplain,
+                child: SearchTextField(
+                  onChangeKeyword: _vm.onChangeKeyword,
+                  onSubmitted: (_) => _vm.onSearch(context),
+                  onSelectSortMethod: () => _vm.onSelectSortMethod(context),
+                ),
+              ),
+              const SizedBox(height: 32),
+
+              // 検索ボタン
+              // アプリでは、キーボードが非表示でキーワードが入力済みの時のみ表示される
+              Visibility(
+                visible: visibleWidgetLogic.isButtonVisible,
+                child: Semantics(
                   container: true,
-                  label: AppLocalizations.of(context).searchExplain,
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxWidth: Responsive.breakLarge),
-                    child: SearchTextField(
-                      onChangeKeyword: _vm.onChangeKeyword,
-                      onSubmitted: (_) => _vm.onSearch(context),
-                      onSelectSortMethod: () => _vm.onSelectSortMethod(context),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 32),
-
-                // 検索ボタン
-                // アプリでは、キーボードが非表示でキーワードが入力済みの時のみ表示される
-                Visibility(
-                  visible: visibleWidgetLogic.isButtonVisible,
-                  child: Semantics(
-                    container: true,
-                    label: AppLocalizations.of(context).search,
-                    child: OutlinedButton(
-                      onPressed: () => _vm.onSearch(context),
-                      style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                  label: AppLocalizations.of(context).search,
+                  child: OutlinedButton(
+                    onPressed: () => _vm.onSearch(context),
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Text(
-                          AppLocalizations.of(context).search,
-                          style: Theme.of(context).textTheme.titleMedium!.merge(
-                                TextStyle(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        AppLocalizations.of(context).search,
+                        style: Theme.of(context).textTheme.titleMedium!.merge(
+                              TextStyle(
+                                color: Theme.of(context).colorScheme.secondary,
                               ),
-                        ),
+                            ),
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
