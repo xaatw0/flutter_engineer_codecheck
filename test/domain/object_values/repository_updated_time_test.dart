c5f190cae8f538c91c8f9d38407ec5173d2c23fa
sakushin <sakushin.udemy@gmail.com>
2022-12-17 20:12:08 +0900

    repository_updated_time_test.dartはCodeMagicで動かない。

--- test/domain/object_values/repository_updated_time_test.dart
+++ test/domain/object_values/repository_updated_time_test.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 void main() async {
   final timeOffset = DateTime.now().timeZoneOffset.inHours;
 
-  test('datetime test', skip: true, () async {
+  test('datetime test', () async {
     // 2012年3月4日 5時6分7秒
     final datetime = DateTime(2012, 3, 4, 5, 6, 7);
     expect(datetime.toString(), '2012-03-04 05:06:07.000');
@@ -33,7 +33,7 @@ void main() async {
     );
   });
 
-  test('converter', () async {
+  test('converter', skip: true, () async {
     const converter = RepositoryCreateTimeConverter();
     expect(
       converter.fromJson('2022-12-06T18:37:57Z')(),
