27e83d45232aba91fe97c92adc678d53aed4e3c4
sakushin <sakushin.udemy@gmail.com>
2022-12-20 18:19:01 +0900

    CodeMagicは時差がないということで解決した

--- test/domain/object_values/repository_updated_time_test.dart
+++ test/domain/object_values/repository_updated_time_test.dart
@@ -6,7 +6,8 @@ void main() async {
   final timeOffset = DateTime.now().timeZoneOffset.inHours;
 
   test('datetime test', () async {
-    expect(timeOffset, 0);
+    // 日本なら9，CodeMagicは0
+    //expect(timeOffset, 0);
 
     // 2012年3月4日 5時6分7秒
     final datetime = DateTime(2012, 3, 4, 5, 6, 7);
@@ -15,16 +16,16 @@ void main() async {
 
     expect(
       DateTime.parse('2022-12-06T18:37:57Z'),
-      DateTime(2022, 12, 6, 18, 37, 57).toUtc(),
+      DateTime(2022, 12, 6, 18 + timeOffset, 37, 57).toUtc(),
     );
 
     expect(
-      DateTime(2022, 12, 6, 18, 37, 57).toUtc().toString(),
+      DateTime(2022, 12, 6, 18 + timeOffset, 37, 57).toUtc().toString(),
       '2022-12-06 18:37:57.000Z',
     );
 
     expect(
-      DateTime(2022, 12, 6, 18, 37, 57).toUtc().toIso8601String(),
+      DateTime(2022, 12, 6, 18 + timeOffset, 37, 57).toUtc().toIso8601String(),
       '2022-12-06T18:37:57.000Z',
     );
 
@@ -39,7 +40,7 @@ void main() async {
     const converter = RepositoryCreateTimeConverter();
     expect(
       converter.fromJson('2022-12-06T18:37:57Z')(),
-      DateTime(2022, 12, 6, 18, 37, 57).toUtc(),
+      DateTime(2022, 12, 6, 18 + timeOffset, 37, 57).toUtc(),
     );
     expect(
       converter.toJson(
